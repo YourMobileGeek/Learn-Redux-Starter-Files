4f554eaf62b2851a5e937535627ebdd96007c619
Ariana M. Davis <hello@yourmobilegeek.tech>
2018-05-27 13:37:49 -0400

    updated react content

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { render } from 'react-dom';
 
-//Import css
+import {
+    render
+} from 'react-dom';
+
+// Import css
 import css from './styles/style.styl';
 
-render(<p> hi </p>, document.getElementById(''));
+// Import Components
+import main from './components/main';
+import single from './components/single';
+import photogrid from './components/photogrid';
+
+//Import react router deps 
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'; 
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={main}>
+            <IndexRoute component={photogrid}></IndexRoute>
+            <Route path="/view/:postId" component={single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
